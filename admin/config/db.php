7e8183b79c583fe0c4e6cd9e14645526506c6e16
Ducanh2404 <1951060533@e.tlu.edu.vn>
2022-03-21 00:16:57 +0700

    update

--- admin/config/db.php
+++ admin/config/db.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('hotel-booking-cloud','doadmin','EGZKPZ2itwg3EsuO','defaultdb');
+    $conn = mysqli_connect('hotel-booking-cloud-5kwkx.ondigitalocean.app','doadmin','EGZKPZ2itwg3EsuO','defaultdb');
     if(!$conn){
         die("Không thể kết nối,kiểm tra lại các tham số kết nối");
     }
