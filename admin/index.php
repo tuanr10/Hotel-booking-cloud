7a996bf50cc4c37c200c04cc78740fd758ab5fa0
Ducanh2404 <1951060533@e.tlu.edu.vn>
2022-03-22 14:13:20 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -111,7 +111,7 @@ if (!isset($_SESSION['login_oke']))
                         </tr>
                       </thead>
                       <?php
-                      $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','EGZKPZ2itwg3EsuO','defaultdb');
+                      $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','K3OIpsluD6RJOx9A','defaultdb');
                       if (!$conn) {
                         die("Không thể kết nối,kiểm tra lại các tham số kết nối");
                       }
@@ -173,7 +173,7 @@ if (!isset($_SESSION['login_oke']))
                         </tr>
                       </thead>
                       <?php
-                      $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','EGZKPZ2itwg3EsuO','defaultdb');
+                      $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','K3OIpsluD6RJOx9A','defaultdb');
                       if (!$conn) {
                         die("Không thể kết nối,kiểm tra lại các tham số kết nối");
                       }
--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','EGZKPZ2itwg3EsuO','defaultdb');
+    $conn = mysqli_connect('dbhotelbooking-do-user-11027843-0.b.db.ondigitalocean.com','doadmin','K3OIpsluD6RJOx9A','defaultdb');
     if(!$conn){
         die("Không thể kết nối,kiểm tra lại các tham số kết nối");
     }
